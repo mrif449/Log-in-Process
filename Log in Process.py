1dadcd61ed15ece7bf2d90d603b52ce132761091
Rahadul Islam <85452696+mrif449@users.noreply.github.com>
2021-07-22 19:55:21 +0600

    Update Log in Process.py

--- Log in Process.py
+++ Log in Process.py
@@ -1,11 +1,18 @@
-print("Email/Username/Phone Number:")
+usrnm = "tiny.teen12"
+email = "tiny.teen12@gmail.com"
+phone = "01345228795"
+pswd = "newpass12@"
+print("Username/Email/Phone Number:")
 username = input()
 print("Password:")
-password = input()
-if username == "tiny.teen12" or username == "01345228795" or username == "tiny.teen12@gmail.com":
-    if password == "newpass12@":
-        print("Log in Successful")
-    else:
+password = input()1
+if (username == usrnm or username == email or username == phone) and password == pswd:
+    print("Log in Successful")
+elif (username != usrnm or username != email or username != phone) or password != pswd:
+
+    if (username != usrnm or username != email or username != phone) and password == pswd:
+        print("Incorrect Email/Username/Phone Number")
+    elif (username == usrnm or username == email or username == phone) and password != pswd:
         print("Incorrect Password")
-else:
-    print("Incorrect Email/Username/Phone Number")
+    else:
+        print("Invalid Account")
